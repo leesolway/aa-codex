d52460d695038638f9f06070917595cc132f0aa0
Lee Solway <solway@outlook.com>
2026-05-09 21:50:08 +0100

    bump AA deps

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     ],
     python_requires=">=3.8",
     install_requires=[
-        "django>=3.2,<5.0",
+        "django>=3.2,<6.0",
         "allianceauth>=3.0.0"
     ],
     extras_require={
